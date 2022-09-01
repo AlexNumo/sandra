340ad3e50fb9a7da6cb9c8cce58110b1c8a25677
Alex Numo <korol1511@gmail.com>
2022-09-01 16:24:43 +0300

    Copied

--- src/Components/API/API.jsx
+++ src/Components/API/API.jsx
@@ -1,7 +1,4 @@
-import { useState, useEffect } from 'react';
 const axios = require('axios');
-
-// mongodb+srv://sandra:sandra01111996@contacts.dmwvegx.mongodb.net/contact-pro
 
 const API = ({
         name,
@@ -11,21 +8,6 @@ const API = ({
         age,
         kind,
         sizeLag}) => {
-    //     "name " + name,
-    //     "surname " + surname,
-    //     "tel " + tel,
-    //     "email " + email,
-    //     "age " + age,
-    //     "kind " + kind,
-    //     "sizeLag " + sizeLag
-    // const [data, setData] = useState(null);
-    // const [apiDATA, setApiDATA] = useState([]);
-
-        // fetch('api/contacts')
-        //     .then(response => response.json())
-        //     .then(response => setData(response));
-    // var myJSON = JSON.parse(data); 
-    // console.log(data[0]);
     axios.post('https://whispering-eyrie-00516.herokuapp.com/api/contacts', {
         name: name,
         surname: surname,
@@ -40,7 +22,6 @@ const API = ({
     .catch(function (error) {
         console.log(error);
     });
-    // console.log(data);
     return (
         <div>
             <h2>Відправлено</h2>
