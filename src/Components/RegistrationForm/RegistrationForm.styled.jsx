b36f447cc1efab190b133e49330eb6cace3fedb0
Alex Numo <korol1511@gmail.com>
2022-09-28 16:41:51 +0300

    Testing user 2

--- src/Components/RegistrationForm/RegistrationForm.styled.jsx
+++ src/Components/RegistrationForm/RegistrationForm.styled.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import styled, { css } from 'styled-components';
 import { FiEyeOff, FiEye } from 'react-icons/fi';
 
