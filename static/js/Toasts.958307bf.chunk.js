b6dff783addf44a6169b596d04e47354b206a288
AlexNumo <84464243+AlexNumo@users.noreply.github.com>
2022-11-10 13:30:26 +0000

    Deploying to gh-pages from @ AlexNumo/sandra@9c4a6db9109d456946e67ac8a80cbb6348db4aa8 🚀

---
+++ static/js/Toasts.958307bf.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksandra=self.webpackChunksandra||[]).push([[517],{4264:function(e,s,n){n.r(s),n.d(s,{default:function(){return t}});var r=n(9085),o=(n(5462),n(184));function t(){return(0,o.jsx)(r.Ix,{position:"top-right",autoClose:5e3,hideProgressBar:!1,newestOnTop:!1,closeOnClick:!0,rtl:!1,pauseOnFocusLoss:!0,draggable:!0,pauseOnHover:!0,theme:"colored"})}}}]);
+//# sourceMappingURL=Toasts.958307bf.chunk.js.map
