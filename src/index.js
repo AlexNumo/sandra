1dcfdbbb9e39f33872e50d7f9bdfd21c0b5f1274
Arseniuk Oleksandr <korol1511@gmail.com>
2022-11-14 10:18:19 +0200

    Add route test 7

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import GlobalStyle from 'GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/sandra' forceRefresh='true'>
       <GlobalStyle />
       <Provider store={store}>
         <App />
